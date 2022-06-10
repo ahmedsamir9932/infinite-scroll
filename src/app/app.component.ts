71c8797a027e1812438b507c87310e23082769e6
ahmedsamir9932 <ahmed.samir9932@gmail.com>
2022-06-11 00:40:46 +0200

    infinite scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, scan } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'Angular Material Select Infinite Scroll';
+  total = 4190;
+  // data = Array.from({length: this.total}).map((_, i) => `Option ${i}`);
+  data = [];
+  limit = 10;
+  end = ''
+  offset = 1;
+  options = new BehaviorSubject<any[]>([]);
+  options$: Observable<any[]>;
+
+  constructor(private http: HttpClient) {
+      this.options$ = this.options.asObservable().pipe(
+          scan((acc: any, curr: any) => {
+              return [...acc, ...curr];
+          }, [])
+      );
+  }
+
+  ngOnInit() {
+      this.getNextBatch();
+  }
+
+  getNextBatch() {
+    this.getItems(this.offset, this.limit)
+    // const result = this.data.slice(this.offset, this.offset + this.limit);
+    // this.options.next(result);
+    // this.offset++;
+  }
+  
+  getItems(offset: number, limit: number) {
+    this.http.get('http://gl.ttechapps.info/api/GetItemsDropDown', {params: new HttpParams()
+    .set('Name', '').set('PageNumber', offset).set('PageSize', limit)}
+    ).subscribe((res: any) => {
+      console.log(res);
+      this.data = res.data;
+      console.log('data', this.data);
+      this.options.next(this.data);
+      this.offset++;
+      this.end = res.note
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSelectInfiniteScrollModule} from 'ng-mat-select-infinite-scroll';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSelectInfiniteScrollModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
